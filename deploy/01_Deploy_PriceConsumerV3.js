e05d9ca84b7800a03557ed163366d54f156ef96b
Jarret Moses <jarret.moses@gmail.com>
2021-07-25 13:37:09 -0400

    Completed day 2

--- deploy/01_Deploy_PriceConsumerV3.js
+++ deploy/01_Deploy_PriceConsumerV3.js
@@ -16,6 +16,8 @@ module.exports = async ({
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed']
     }
+
+    // let ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed']
     // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     // Default one below is ETH/USD contract on Kovan
     log("----------------------------------------------------")
